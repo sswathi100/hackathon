dd43e76964f852213f2ab59889af17fc2063589c
sswathi100 <swathisendhil@gmail.com>
2019-08-24 14:44:18 -0400

    added camera and bar graph

--- app/src/main/java/com/example/ssendhil/dream_team/Main3Activity.java
+++ app/src/main/java/com/example/ssendhil/dream_team/Main3Activity.java
@@ -1,16 +1,25 @@
 package com.example.ssendhil.dream_team;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.support.v4.app.Fragment;
+import android.widget.Toast;
 
-public class Main3Activity extends AppCompatActivity {
+import java.io.ByteArrayOutputStream;
+
+public class Main3Activity extends AppCompatActivity implements Stats.OnFragmentInteractionListener {
 
     private TextView mTextMessage;
+    private static final int Image_Capture_Code = 1;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -21,22 +30,22 @@ public class Main3Activity extends AppCompatActivity {
 
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    // replace with: fragment = new MyFragment()
+                    Intent cInt = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(cInt,Image_Capture_Code);
                     return true;
+
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    // replace with: fragment = new MyFragment()
-                    return true;
+                    // mTextMessage.setText(R.string.title_dashboard);
+                    fragment = new Stats();
+                    break;
                 case R.id.navigation_notifications:
                     mTextMessage.setText(R.string.title_notifications);
                     // replace with: fragment = new MyFragment()
                     return true;
             }
 
-            //getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, fragment).commit();
-
-            return false;
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, fragment).commit();
+                return true;
         }
     };
 
@@ -50,4 +59,24 @@ public class Main3Activity extends AppCompatActivity {
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == Image_Capture_Code) {
+            if (resultCode == RESULT_OK) {
+                Bitmap bp = (Bitmap) data.getExtras().get("data");
+                // convert to byte array
+                ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                bp.compress(Bitmap.CompressFormat.PNG, 100, stream);
+                byte[] byteArray = stream.toByteArray();
+
+            } else if (resultCode == RESULT_CANCELED) {
+                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+        // TODO:
+    }
 }
---
+++ app/src/main/java/com/example/ssendhil/dream_team/Stats.java
@@ -0,0 +1,143 @@
+package com.example.ssendhil.dream_team;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link Stats.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link Stats#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class Stats extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    private OnFragmentInteractionListener mListener;
+
+    public Stats() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment Stats.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static Stats newInstance(String param1, String param2) {
+        Stats fragment = new Stats();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_stats, container, false);
+
+        BarChart chart = (BarChart) view.findViewById(R.id.bar_chart);
+        ArrayList<BarEntry> BarEntry = new ArrayList<>();
+
+        BarEntry.add(new BarEntry(2f, 0));
+        BarEntry.add(new BarEntry(4f, 1));
+        BarEntry.add(new BarEntry(6f, 2));
+        BarEntry.add(new BarEntry(8f, 3));
+        BarEntry.add(new BarEntry(7f, 4));
+        BarEntry.add(new BarEntry(3f, 5));
+
+        BarDataSet dataSet = new BarDataSet(BarEntry, "Projects");
+        ArrayList<String> labels = new ArrayList<>();
+        labels.add("January");
+        labels.add("February");
+        labels.add("March");
+        labels.add("April");
+        labels.add("May");
+        labels.add("June");
+
+        BarData data = new BarData(dataSet);
+        dataSet.setColors(ColorTemplate.COLORFUL_COLORS);
+        chart.setData(data);
+
+        chart.getDescription().setText("Description of my chart");
+
+        return view;
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}
